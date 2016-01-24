0e7142ded2a43f999cc67c3513d4accc15dc24ae
patricksun <patricksun@kkbox.com>
2016-01-24 23:11:33 +0800

    first commit?

---
+++ mediunion_2/Models/ScheduleEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mediunion_2.Models
+{
+    public class ScheduleEntry
+    {
+        public int EntryId { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string AgentName { get; set; }
+        public string FacilityName { get; set; }
+        public string CustomerName { get; set; }
+        public string CallNotes { get; set; }   // Call Notes
+        public string GeneralFee { get; set; } // 費用。
+        public string TransportationFee { get; set; }   // 交通費。
+
+        // 備註及單據說明： 常用代號：  P - 停車費 O - 油資  E - ETC費用
+        public string Remarks { get; set; }
+    }
+}
---
+++ mediunion_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mediunion_2.Startup))]
+namespace mediunion_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
